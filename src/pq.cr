8e63644714cd39762f2fbe67c3f14dedf5cfaf0b
Justin Bradford <jabradford@gmail.com>
2025-05-09 00:11:33 -0700

    Working feedback response on received/replicated LSN marker

--- src/pq.cr
+++ src/pq.cr
@@ -1,5 +1,12 @@
 require "./lib_pq"
 
+require "log"
+# @todo: switch back to async default once we're running
+# in an isolated exec context.
+Log.setup do
+  Log::IOBackend.new dispatcher: :sync
+end
+
 class PQ
   @connection : Pointer(LibPQ::PGconn)
   @slot : String
@@ -21,9 +28,40 @@ class PQ
     end
   end
 
+  struct LSN
+    getter value
+    @value = 0_u64
+
+    def initialize(from : String)
+      hi, lo = from.split('/')
+      @value = hi.to_u64 << 32 | lo.to_u64
+    end
+
+    def initialize(@value : UInt64)
+    end
+
+    def to_s : String
+      "#{@value >> 32}/#{@value & 0xFFFFFFFF}"
+    end
+
+    def max(other : UInt64)
+      @value = Math.max(@value, other)
+      self
+    end
+  end
+
+  private module PGTime
+    private PG_EPOCH = Time.utc(2000, 1, 1)
+
+    def self.now : Int64
+      (Time.utc - PG_EPOCH).total_microseconds.to_i64
+    end
+  end
+
   def initialize(config : Config)
     @slot = config.slot
     @publication = config.publication
+    @lsn = LSN.new("0/0")
 
     dsn = [
       config.host.presence && "host=#{config.host}",
@@ -49,8 +87,11 @@ class PQ
   end
 
   def start(lsn : String)
+    @lsn.max(LSN.new(lsn).value)
+    puts("Starting LSN: #{@lsn.to_s}")
+
     start_command = "
-      START_REPLICATION SLOT #{@slot} LOGICAL #{lsn}
+      START_REPLICATION SLOT #{@slot} LOGICAL #{@lsn.to_s}
       (proto_version '2', publication_names '#{@publication}')
     "
 
@@ -77,15 +118,23 @@ class PQ
       bytes_read = LibPQ.get_copy_data(@connection, pointerof(buffer), 0)
 
       if bytes_read > 0
-        # @todo
-        puts(buffer)
+        # https://www.postgresql.org/docs/current/protocol-replication.html#PROTOCOL-REPLICATION-XLOGDATA
         data = Slice.new(buffer.as(Pointer(UInt8)), bytes_read)
-        type = data[0].chr
-        puts(type)
-        lsn = lsn_to_string(IO::ByteFormat::BigEndian.decode(UInt64, data[1..8]))
-        puts(lsn)
+        message_type = data[0].chr
+        case message_type
+        when 'w' # WAL data
+          puts("w")
+          lsn = IO::ByteFormat::BigEndian.decode(UInt64, data[9..16])
+          process_message(data[25..])
+          # @todo: move this into process message when we actually send data to sink
+          # @lsn.max(lsn)
+          feedback(lsn)
 
-        feedback(lsn)
+        when 'k' # Keepalive message
+          puts("k")
+          lsn = IO::ByteFormat::BigEndian.decode(UInt64, data[1..8])
+          feedback(lsn)
+        end
         LibPQ.freemem(buffer)
 
       elsif bytes_read < 0
@@ -95,30 +144,42 @@ class PQ
     end
   end
 
-  private def lsn_to_string(lsn : UInt64) : String
-    "#{lsn >> 32}/#{lsn & 0xFFFFFFFF}"
+  private def process_message(message : Slice(UInt8))
+    # https://www.postgresql.org/docs/17/protocol-logicalrep-message-formats.html
+    type = message[0].chr
+    puts(type)
+    puts(message)
   end
 
-  private def feedback(lsn : String)
-    # @todo:
-    # r, wal+1, wal+1, wal+1, time in pg epoch, 0
+  private def feedback(lsn : UInt64)
+    puts("Received LSN: #{lsn}")
+    puts("Replicated LSN: #{@lsn.to_s}")
+
+    # https://www.postgresql.org/docs/current/protocol-replication.html#PROTOCOL-REPLICATION-STANDBY-STATUS-UPDATE
+    feedback = IO::Memory.new(34)
+    feedback.write_byte('r'.ord.to_u8) # Standby status update (r)
+    feedback.write_bytes(lsn, IO::ByteFormat::BigEndian) # received
+    feedback.write_bytes(@lsn.value, IO::ByteFormat::BigEndian) # flushed
+    feedback.write_bytes(@lsn.value, IO::ByteFormat::BigEndian) # applied
+    feedback.write_bytes(PGTime.now, IO::ByteFormat::BigEndian)
+    feedback.write_byte(0_u8) # no reply
+
+    data = feedback.to_slice
     result = LibPQ.put_copy_data(
       @connection,
-      lsn.to_unsafe.as(LibC::Char*),
-      lsn.size
+      data.to_unsafe.as(LibC::Char*),
+      data.size
     )
-    puts("feedback", result)
+
     if result != 1
       error_msg = String.new(LibPQ.error_message(@connection))
       raise "Failed to send feedback: #{error_msg}"
     end
 
-    # puts("end")
-    # result = LibPQ.put_copy_end(@connection, Pointer(LibC::Char).null)
-    # if result != 1
-    #   error_msg = String.new(LibPQ.error_message(@connection))
-    #   raise "Failed to flush feedback: #{error_msg}"
-    # end
+    if LibPQ.flush(@connection) != 0
+      error_msg = String.new(LibPQ.error_message(@connection))
+      raise "Failed to flush connection: #{error_msg}"
+    end
   end
 end
 
